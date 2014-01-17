7340013ca0d6eff5dd4e6b59167d552de7932027
Mani Bhushan <manibhushan.cs@gmail.com>
2014-01-17 15:41:33 +0530

    linked list representation of stack with iterator

---
+++ 13_StackLinkedList/src/Stack.java
@@ -0,0 +1,64 @@
+import java.util.Iterator;
+
+
+public class Stack<Item> implements Iterable<Item> {
+	private Node first;
+	private int N;
+	
+	private class Node {
+		Item item;
+		Node next;
+	}
+	
+	public Stack() {
+		first = null;
+		N = 0;
+	}
+	
+	public void push(Item item) {
+		Node node = new Node();
+		node.item = item;
+		
+		node.next = first;
+		first = node;
+		N++;
+	}
+	
+	public Item pop() {
+		if (isEmpty()) { return null; }
+		
+		Item item = first.item;
+		first = first.next;
+		
+		N--;
+		return item;
+	}
+	public boolean isEmpty() { return (null == first); }
+	public int size() { return N; }
+
+	@Override
+	public Iterator<Item> iterator() {
+		return new ListIterator();
+	}
+	
+	private class ListIterator implements Iterator<Item> {
+		
+		private Node current = first;
+
+		@Override
+		public boolean hasNext() {
+			return (null != current);
+		}
+
+		@Override
+		public Item next() {
+			Item val = current.item;
+			current = current.next;
+			return val;
+		}
+
+		@Override
+		public void remove() {
+		}
+	}
+}
