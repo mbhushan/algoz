7e3c39bbe5bcdd1e3f92565ce6d0e0c2b0ebd68c
Mani Bhushan <manibhushan.cs@gmail.com>
2021-08-31 09:07:59 -0700

    Given the root of a binary tree, return its maximum depth.

--- toplc_trees/src/MaxDepthTree.java
+++ toplc_trees/src/MaxDepthTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  https://leetcode.com/explore/featured/card/top-interview-questions-easy/94/trees/555/
  */
@@ -30,5 +33,26 @@ public class MaxDepthTree {
         int rdepth = maxDepth(root.right);
 
         return 1 + Math.max(ldepth, rdepth);
+    }
+
+    public int maxDepthBFS(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int depth = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            depth++;
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+        }
+
+        return depth;
     }
 }
