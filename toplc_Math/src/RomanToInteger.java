9292cb02212a6cf759e40945c70258bde36b01ff
Mani Bhushan <manibhushan.cs@gmail.com>
2021-09-06 09:38:57 -0700

    Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.

---
+++ toplc_Math/src/RomanToInteger.java
@@ -0,0 +1,43 @@
+/**
+ https://leetcode.com/explore/featured/card/top-interview-questions-easy/102/math/878/discuss
+ */
+
+public class RomanToInteger {
+
+    public static void main(String[] args) {
+        RomanToInteger ri = new RomanToInteger();
+        String s = "MCMXCIV";
+        System.out.println(ri.romanToInt(s));
+    }
+
+    public int romanToInt(String s) {
+        int res = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            switch (c) {
+                case 'I':
+                    res += (res >= 5 ? -1 : 1);
+                    break;
+                case 'V':
+                    res += 5;
+                    break;
+                case 'X':
+                    res += 10 * (res >= 50 ? -1 : 1);
+                    break;
+                case 'L':
+                    res += 50;
+                    break;
+                case 'C':
+                    res += 100 * (res >= 500 ? -1 : 1);
+                    break;
+                case 'D':
+                    res += 500;
+                    break;
+                case 'M':
+                    res += 1000;
+                    break;
+            }
+        }
+        return res;
+    }
+}
