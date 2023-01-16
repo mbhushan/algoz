391f001f11bb6fb02d0aa850c05fce6dbe74c3ef
Mani Bhushan <manibhushan.cs@gmail.com>
2023-01-15 17:05:53 -0800

    LC training

---
+++ 0DailyLC/src/SubarraySum.java
@@ -0,0 +1,10 @@
+public class SubarraySum {
+    public static void main(String[] args) {
+        SubarraySum ss = new SubarraySum();
+
+    }
+
+    public int subarraySum(int[] nums, int k) {
+        
+    }
+}
